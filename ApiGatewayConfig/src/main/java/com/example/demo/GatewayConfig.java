638c29f785ee13cca5b3559dcf725127580c436c
parimellarahul <parimellarahul@gmail.com>
2025-11-30 19:34:25 +0530

    Revert "added pagination"
    This reverts commit 28ee5dce220e27749f8e5f0bb030cb808e9cdb8c.

--- ApiGatewayConfig/src/main/java/com/example/demo/GatewayConfig.java
+++ ApiGatewayConfig/src/main/java/com/example/demo/GatewayConfig.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.filter.AuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -11,24 +9,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-        @Autowired
-        private AuthenticationFilter filter;
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // ✅ Route for Order Microservice — uses Eureka for load balancing
+                .route("ORDER-MICROSERVICE", r -> r
+                        .path("/orders/**")
+                        .uri("lb://ORDER-MICROSERVICE"))
 
-        @Bean
-        public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-                return builder.routes()
-                                // ✅ Route for Order Microservice — uses Eureka for load balancing
-                                .route("ORDER-MICROSERVICE", r -> r
-                                                .path("/orders/**")
-                                                .filters(f -> f.filter(filter.apply(new AuthenticationFilter.Config())))
-                                                .uri("lb://ORDER-MICROSERVICE"))
+                // ✅ Route for Product Microservice
+                .route("PRODUCT-MICROSERVICE", r -> r
+                        .path("/products/**")
+                        .uri("lb://PRODUCT-MICROSERVICE"))
+                .build();
+    }
 
-                                // ✅ Route for Product Microservice
-                                .route("PRODUCT-MICROSERVICE", r -> r
-                                                .path("/products/**")
-                                                .filters(f -> f.filter(filter.apply(new AuthenticationFilter.Config())))
-                                                .uri("lb://PRODUCT-MICROSERVICE"))
-                                .build();
-        }
+
 
 }
